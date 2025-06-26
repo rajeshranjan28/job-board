f3f101d08857d2408440e3a6c150ff6ba8280033
rajeshranjan28 <rajeshranjan2803@gmail.com>
2025-06-26 11:56:00 +0530

    jobs

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
 
-export { default as Logo } from './logo';
-export { default as FormRow} from './FormRow'
-export { default as Navbar} from './Navbar'
-export { default as BigSidebar} from './BigSidebar'
-export { default as SmallSidebar} from './SmallSidebar'
+export { default as Logo } from './logo.jsx';
+export { default as FormRow} from './FormRow.jsx'
+export { default as Navbar} from './Navbar.jsx'
+export { default as BigSidebar} from './BigSidebar.jsx'
+export { default as SmallSidebar} from './SmallSidebar.jsx'
 
 
 
