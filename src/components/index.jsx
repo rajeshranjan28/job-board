a240a644dd7c295214ed4bce328f60621e6f6f9c
rajeshranjan28 <rajeshranjan2803@gmail.com>
2025-06-26 13:20:03 +0530

    .

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,4 @@
 
-export { default as Logo } from './logo';
 export { default as FormRow} from './FormRow'
 export { default as Navbar} from './Navbar'
 export { default as BigSidebar} from './BigSidebar'
